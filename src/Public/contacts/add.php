19d48639aa7ecfec2586e8526939788e52ac026f
jun <juntesemail@gmail.com>
2024-03-11 22:28:31 +0700

    feat: add title

--- src/Public/contacts/add.php
+++ src/Public/contacts/add.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/output.css">
     <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon">
-    <title>UnityBook</title>
+    <title>UnityBook - Create Contact</title>
 </head>
 
 <body class="flex flex-col min-h-screen min-w-full bg-[url('../images/bg.png')] bg-cover font-inter">
