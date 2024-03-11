3bd0b443f4e8b863c048c0712e86dcfa1d7f58ee
jun <juntesemail@gmail.com>
2024-03-11 22:14:11 +0700

    feat: add wallpaper

--- src/Public/contacts/add.php
+++ src/Public/contacts/add.php
@@ -1,1 +1,20 @@
-<!-- jun -->
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/output.css">
+    <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon">
+    <title>UnityBook</title>
+</head>
+
+<body class="flex flex-col min-h-screen min-w-full bg-[url('../images/bg.png')] bg-cover font-inter">
+    <div class="flex flex-col flex-1 justify-center items-center">
+
+        
+    </div>
+
+</body>
+
+</html>
