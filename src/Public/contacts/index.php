a9db34f1624830615fcf99f7e9ba0bd7161df310
rdhwan <fathanridhwann@gmail.com>
2024-03-12 10:47:48 +0700

    feat: contact page

--- src/Public/contacts/index.php
+++ src/Public/contacts/index.php
@@ -14,9 +14,22 @@
     <div class="flex flex-col h-96 p-6 md:p-8 md:px-64 gap-6 justify-evenly">
         <div class="flex flex-row items-center justify-between">
             <h1 class="text-2xl font-bold">Contact</h1>
-            <button class="btn btn-circle bg-[#DFF5FF]">
-                <img src="../images/icons/profile.svg" alt="my profile" />
-            </button>
+
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-circle bg-[#DFF5FF]">
+                    <img src="../images/icons/profile.svg" alt="my profile" />
+                </div>
+                <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 gap-2">
+                    <li class="p-4">Username</li>
+                    <hr />
+                    <li>
+                        <a class="flex flex-row justify-between text-red-500">
+                            Log out
+                            <i class="ph-bold ph-sign-out"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
 
         <label class="input w-full bg-[#D9D9D9] flex items-center gap-2 rounded-full">
@@ -54,11 +67,17 @@
             <div class="dropdown">
                 <div tabindex="0" role="button" class="btn btn-ghost px-0 pr-2 text-[#5356FF]">
                     <i class="ph ph-faders-horizontal text-xl"></i>
-                    Filter
+                    Sort
                 </div>
                 <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
+                    <li>
+                        <a class="flex flex-row justify-between font-bold">
+                            A-Z <i class="ph-bold ph-sort-ascending"></i></a>
+                    </li>
+                    <li>
+                        <a class="flex flex-row justify-between font-bold">
+                            Z-A <i class="ph-bold ph-sort-descending"></i></a>
+                    </li>
                 </ul>
             </div>
 
@@ -92,8 +111,33 @@
                         <i class="ph-fill ph-dots-three-outline-vertical"></i>
                     </div>
                     <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
+                        <li>
+                            <a class="flex flex-row justify-between text-blue-500">
+                                Edit User
+                                <i class="ph ph-pencil-simple-line"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="flex flex-row justify-between text-red-500">
+                                Delete User
+                                <i class="ph ph-trash"></i>
+                            </a>
+                        </li>
+
+                        <hr>
+                        <li>
+                            <a class="flex flex-row justify-between text-yellow-500">
+                                Add to Favourites
+                                <i class="ph ph-star"></i>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a class="flex flex-row justify-between text-red-400">
+                                Remove from Favourites
+                                <i class="ph ph-star"></i>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -103,7 +147,7 @@
         <?php endfor ?>
 
 
-        <a href="./add.php" class="fixed bottom-0 right-0 btn btn-circle w-16 h-16 m-4">
+        <a href="./add.php" class="fixed bottom-0 right-0 btn btn-circle w-16 h-16 m-8">
             <i class="ph ph-user-circle-plus text-4xl"></i>
         </a>
 
